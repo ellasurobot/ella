fc8ea279978a2c06d3a42763e67f2d22539fc852
Ellasurobot <ellasurobot@gmail.com>
2014-01-31 12:45:34 +0000

    refactor

--- Moves.py
+++ Moves.py
@@ -1,15 +1,16 @@
 from BrickPi import *   #import BrickPi.py file to use BrickPi operations
+from RobotSettings import *
 
 def forward(distance_cm):
 	BrickPiUpdateValues() 
 	curr_degree = BrickPi.Encoder[PORT_A]
-	BrickPi.MotorSpeed[PORT_A] = FORWARD_SPEED_A   
-	BrickPi.MotorSpeed[PORT_B] = FORWARD_SPEED_B    
+	BrickPi.MotorSpeed[PORT_A] = FORWARD_SPEED   
+	BrickPi.MotorSpeed[PORT_B] = FORWARD_SPEED    
 	while (BrickPi.Encoder[PORT_A] - curr_degree < 980):
     		BrickPiUpdateValues() 
 
-def turn(degrees):
-    	BrickPiUpdateValues() 
+def turn_right(degrees):
+ 	BrickPiUpdateValues() 
 	BrickPi.MotorSpeed[PORT_A] = TURN_SPEED   
 	BrickPi.MotorSpeed[PORT_B] = -1*TURN_SPEED    
 	if(degrees < 0):
