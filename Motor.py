79efd24eda9e217e98b3b24a4a71330d1f79480b
Haixiao Su <hs2711@ic.ac.uk>
2014-02-01 03:23:23 +0000

    Refactored a bit.

--- Motor.py
+++ Motor.py
@@ -4,6 +4,7 @@ class Motor:
 	def __init__(self, port):
 		self.__port = port
 		BrickPi.MotorEnable[__port] = 1     #Enable the Motor
+		self.__initial_rotation = 0
 
 	def get_speed():
 		return BrickPi.MotorSpeed[self.__port]
@@ -14,3 +15,8 @@ class Motor:
 	def get_current_rotation():
 		return BrickPi.Encoder[self.__port]
 
+	def get_initial_rotation():
+		return self.__initial_rotation
+
+	def set_initial_rotation():
+		self.__initial_rotation = get_current_rotation()
--- Robot.py
+++ Robot.py
@@ -1,4 +1,6 @@
 from Motor import *
+from MotorSettings import*
+import math
 
 class Robot:
 	def __init__(self):
@@ -11,6 +13,48 @@ class Robot:
 		BrickPiUpdateValues() 
 
 	def forward(distance):  # distance is in cm
+		self.__motorA.set_speed(FORWARD_SPEED_A)
+		self.__motorB.set_speed(FORWARD_SPEED_B)
+		run_motor(self.__motorA, self.__motorB, ROTATIONS_PER_CM * distance_cm, "forward")
 
+	def turn(degrees): #degrees in encoder degree
+		index = (degrees - abs(degrees)) + 1
+		self.__motorA.set_speed(index * TURN_SPEED)
+		self.__motorB.set_speed(-1 * index * TURN_SPEED_B)
+		run_motor(self.__motorA, self.__motorB, ROTATIONS_PER_DEGREE * degrees, "turn")		
 
+	def run_motor(reference_motor, other_motor, degrees_to_turn, movement):
+		initial_rotation = reference_motor.update_and_return_initial_rotation()
+		initial_time = time.time()
+		while (reference_motor.get_current_rotation() - initial_rotation < degress_to_turn):
+			adjust_speed(reference_motor, other_motor, initial_time, movement)
+			initial_time = time.time()
+			BrickPiUpdateValues()
 
+	# Adjust the speed of other motor according to the reference motor
+	# using the PID algorithm
+	def adjust_speed(reference_motor, other_motor, initial_time, movement):
+		time_difference = time.time() - initial_time
+		if (time_difference > TIME_DIFF):
+			adjustment = control(reference_motor, other_motor, initial_time, movement)
+			other_motor.set_speed(other_motor.get_speed() + adjustment)
+			reference_motor.update_and_return_initial_rotation()
+			other_motor.update_and_return_initial_rotation()
+
+	def control(reference_motor, other_motor, initial_time, movement):
+		time_difference = time.time() - initial_time
+		speed_a = get_speed(reference_motor)
+		speed_b = get_speed(other_motor)
+		error = speed_a - speed_b
+		derivative = (error - ERROR)/time_difference
+		integral = math.fabs(reference_motor.get_current_rotation() - reference_motor.get_initial_rotation()) - math.fabs(other_motor.get_current_rotation() - other_motor.get_initial_rotation())
+		k_p, k_i , k_d = get_constants(movement)
+		out = k_p * error + k_i * integral + k_d * derivative
+		other_motor.set_speed(other_motor.get_speed() + out)
+		ERROR = error
+
+	def get_constants(movement):
+		return {
+			"forward": (K_p_f, K_i_f, K_d_f),
+			"turn": (K_p_t, K_i_t, K_d_t)
+			}[movement]
