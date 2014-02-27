34fddd6b5a740c4390e92a3cffa34cc7ae5d4b03
Ellasurobot <ellasurobot@gmail.com>
2014-02-27 16:37:41 +0000

    moved provided code to RobotNav

--- CircularScan.py
+++ CircularScan.py
@@ -2,6 +2,7 @@ from RobotNav import *
 import sys
 import random
 import math
+from place_rec_bits import *
 
 robot = RobotNav()
 #robot.turn_sonar(90)
@@ -10,12 +11,6 @@ robot = RobotNav()
 
 centre = (400,400)
 
-for degree in range(360):
-	robot.turn_sonor(1)
-#	reading = random.randint(0,255)
-	reading = robot.get_sonar_value()
-	x = centre[0] + reading * math.cos(math.radians(degree))
-	y = centre[1] + reading * math.sin(math.radians(degree))
-	print "drawLine:" + str(centre + (x,y))
+robot.learn_location()
 	
 
