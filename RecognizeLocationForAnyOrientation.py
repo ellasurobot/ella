40881632e2d1aa78a1ef978a5fdcc40ecf860f29
Ellasurobot <ellasurobot@gmail.com>
2014-02-28 18:30:20 +0000

    Added new file for any orientation.

---
+++ RecognizeLocationForAnyOrientation.py
@@ -0,0 +1,14 @@
+from RobotNav import *
+import sys
+import random
+import math
+from place_rec_bits import *
+
+robot = RobotNav()
+#robot.turn_sonar(90)
+
+#print "drawParticles:" + str([(,0,0)] )
+
+centre = (400,400)
+
+robot.recognize_location_for_any_orientation()
